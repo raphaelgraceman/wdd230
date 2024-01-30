c84ef21f85408eed60115f79ee1990f63e9341df
nuturethemyoungwell <raphaelprominence10@gmail.com>
2024-01-30 14:07:49 +0000

    Added dark mode to home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -13,3 +13,33 @@ currentYearElement.textContent = currentYear;
 //Get current Date
 let currentDateElement = document.getElementById("currentDate");
 currentDateElement.textContent = currenDate;
+
+
+//Hamburger Menu Items
+//store the selected elements that we are going to use.
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+//Add a click event istener to the hamburger button and 
+// use a callback function that toggles the list element's 
+// list of classes
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+//Toggle Dark Mode 
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
