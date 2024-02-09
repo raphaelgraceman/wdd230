362852f257983e466cca0e92ff67a4b77bdc5618
nuturethemyoungwell <raphaelprominence10@gmail.com>
2024-02-09 06:22:29 +0000

    Weather

---
+++ week05/scripts/weather.js
@@ -0,0 +1,6 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/___________';
