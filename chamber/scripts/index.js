7e5d365e59e9aa786fd5d8363a214530c4c92ce3
nuturethemyoungwell <raphaelprominence10@gmail.com>
2024-02-18 15:50:13 +0000

    amending some changes to the chamber directory

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -77,9 +77,49 @@ document.addEventListener("DOMContentLoaded", function() {
   
 
   //Timestamp
-  document.getElementById('timestamp').value = new Date().toISOString();
+  document.getElementById("timestamp").value = new Date().toISOString();
 
 
 
+  //WEATHER
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+//My API key
+const apiKey = "18bb7e5325abcfee63c18693a2332ed9";
+const imperial = "imperial";
+//Request URL
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${5.53}&lon=${-0.42}&units=${imperial}&appid=${apiKey}`;
+
+//Asynchronous function
+
+// Use OpenWeatherMap API
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // Output data to the console for testing
+      // Extract and display specific weather data
+      currentTemp.textContent = data.main.temp;
+      captionDesc.textContent = data.weather[0].description;
+      const iconCode = data.weather[0].icon;
+      weatherIcon.src = `https://openweathermap.org/img/w/${iconCode}.png`;
+    } else {
+      throw new Error(await response.text());
+    }
+  } catch (error) {
+    console.error(error); // Output any try error to the console
+  }
+}
+
+// Invoke the Function
+apiFetch();
+
+
+//Chamber Home Page Banner
+const banner = document.getElementById("banner");
 
         
